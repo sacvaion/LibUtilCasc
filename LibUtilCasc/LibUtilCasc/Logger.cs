05e5e845a1f5e11f3b00be943e8e1a8701c3a98e
Carlos Siabato <sacvaion@gmail.com>
2026-04-13 15:03:43 -0500

    perf: Logger.RemoverSignosAcentos() use StringBuilder instead of string concatenation

--- LibUtilCasc/LibUtilCasc/Logger.cs
+++ LibUtilCasc/LibUtilCasc/Logger.cs
@@ -3,42 +3,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace LibUtilCasc
 {
+    /// <summary>
+    /// Clase de logging que encapsula NLog con utilidades para remover acentos
+    /// </summary>
     public class Logger
     {
+        private static readonly NLog.Logger _log = LogManager.GetCurrentClassLogger();
+
         private const string ConSignos = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇçÑñ";
         private const string SinSignos = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCcNn";
 
         /// <summary>
-        /// Elimina tildes para la impresion en archivo plano
+        /// Elimina tildes para la impresion en archivo plano (optimizado con StringBuilder)
         /// </summary>
-        /// <param name="texto"></param>
-        /// <returns></returns>
+        /// <param name="texto">Texto con posibles acentos</param>
+        /// <returns>Texto sin acentos</returns>
         public static string RemoverSignosAcentos(string texto)
         {
-            var textoSinAcentos = string.Empty;
+            if (string.IsNullOrEmpty(texto))
+                return texto;
+
+            var textoSinAcentos = new StringBuilder();
 
             foreach (var caracter in texto)
             {
                 var indexConAcento = ConSignos.IndexOf(caracter);
                 if (indexConAcento > -1)
-                    textoSinAcentos = textoSinAcentos + (SinSignos.Substring(indexConAcento, 1));
+                    textoSinAcentos.Append(SinSignos.Substring(indexConAcento, 1));
                 else
-                    textoSinAcentos = textoSinAcentos + (caracter);
+                    textoSinAcentos.Append(caracter);
             }
-            return textoSinAcentos;
+            return textoSinAcentos.ToString();
         }
 
         /// <summary>
-        ///  Escribe el msj enviado con la fecha de proceso
+        /// Escribe un mensaje de información
+        /// </summary>
+        /// <param name="msj">Mensaje a registrar</param>
+        public static void Info(string msj)
+        {
+            if (!string.IsNullOrEmpty(msj))
+            {
+                msj = RemoverSignosAcentos(msj);
+                _log.Info(msj);
+            }
+        }
+
+        /// <summary>
+        /// Escribe un mensaje de advertencia
+        /// </summary>
+        /// <param name="msj">Mensaje a registrar</param>
+        public static void Warn(string msj)
+        {
+            if (!string.IsNullOrEmpty(msj))
+            {
+                msj = RemoverSignosAcentos(msj);
+                _log.Warn(msj);
+            }
+        }
+
+        /// <summary>
+        /// Escribe un mensaje de error
+        /// </summary>
+        /// <param name="msj">Mensaje a registrar</param>
+        /// <param name="ex">Excepción asociada (opcional)</param>
+        public static void Error(string msj, Exception ex = null)
+        {
+            if (!string.IsNullOrEmpty(msj))
+            {
+                msj = RemoverSignosAcentos(msj);
+                if (ex != null)
+                    _log.Error(ex, msj);
+                else
+                    _log.Error(msj);
+            }
+        }
+
+        /// <summary>
+        /// [Obsoleto] Escribe el msj enviado con la fecha de proceso. Use Info() en su lugar.
         /// </summary>
         /// <param name="msj"></param>
+        [Obsolete("Use Logger.Info() instead.", false)]
         public static void EscribirMsj(String msj)
         {
-            msj = RemoverSignosAcentos(msj);
-            Console.WriteLine("[" + DateTime.Now.ToString() + "] --->" + msj);
+            Info(msj);
         }
     }
 }
